d5977155cd463a9b82ae734ed6e5df75780478f4
acumplid <alexandre.cumplido@bsc.es>
2025-05-01 10:47:15 +0200

    Formatter

--- src/App.js
+++ src/App.js
@@ -1,59 +1,60 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 import search from "./assets/images/search.svg";
 import { useAxios } from "./hooks/useAxios";
 import file from "./assets/images/file.svg";
 
 function App() {
-
   const [results, setResults] = useState([]);
 
-	const [inputValue, setInputValue] = useState('');
-	const { getRepoData } = useAxios();
-  const [ filteredList, setFilteredList ] = useState({})
+  const [inputValue, setInputValue] = useState("");
+  const { getRepoData } = useAxios();
+  const [filteredList, setFilteredList] = useState({});
 
-	const handleSubmit = (event) => {
-		event.preventDefault();
-		getRepoData(inputValue).then(results => setResults(results))
-	}
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    getRepoData(inputValue).then((results) => setResults(results));
+  };
 
-	const groupItems = (items) => {
-		const uniqs = items.reduce((acc, val) => {
-			acc[val] = acc[val] === undefined ? 1 : acc[val] += 1;
-			return acc;
-		}, {});
-		setFilteredList(uniqs)
-	}
+  const groupItems = (items) => {
+    const uniqs = items.reduce((acc, val) => {
+      acc[val] = acc[val] === undefined ? 1 : (acc[val] += 1);
+      return acc;
+    }, {});
+    setFilteredList(uniqs);
+  };
 
-	useEffect(() => {
-		results.length > 0 && groupItems(results)
-	}, [results]);
+  useEffect(() => {
+    results.length > 0 && groupItems(results);
+  }, [results]);
 
   return (
     <div>
       <header>
-       <h2>Github Extension Counter</h2>
+        <h2>Github Extension Counter</h2>
         <p>Añade la URL del repositorio que quieras analizar</p>
       </header>
       <form onSubmit={handleSubmit}>
-          <input value={inputValue} onChange={(event) => setInputValue(event.target.value)} placeholder="https://api.github.com/repos/[USER]/[REPO]"/>
-          <button type="submit">
-            <img src={search} width={21} height={21} alt="Buscar" />
-          </button>
-    </form>
-    <div>
-			{ Object.keys(filteredList).length > 0 &&
-				Object.keys(filteredList).map((key, index) => (
-						<div key={index}>
-							<div>
-								<img src={file} width={50} height={40} alt="Buscar" />
-								<div>{key}</div>
-							</div>
-							<div>{filteredList[key]}</div>
-						</div>
-					)
-				)
-			 }
-		</div>
+        <input
+          value={inputValue}
+          onChange={(event) => setInputValue(event.target.value)}
+          placeholder="https://api.github.com/repos/[USER]/[REPO]"
+        />
+        <button type="submit">
+          <img src={search} width={21} height={21} alt="Buscar" />
+        </button>
+      </form>
+      <div>
+        {Object.keys(filteredList).length > 0 &&
+          Object.keys(filteredList).map((key, index) => (
+            <div key={index}>
+              <div>
+                <img src={file} width={50} height={40} alt="Buscar" />
+                <div>{key}</div>
+              </div>
+              <div>{filteredList[key]}</div>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,47 +1,46 @@
-import axios from 'axios';
-
+import axios from "axios";
 
 export const useAxios = () => {
-	const access_token = process.env.REACT_APP_GITHUB_TOKEN;
+  const access_token = process.env.REACT_APP_GITHUB_TOKEN;
 
-	const files = []
+  const files = [];
 
-	const getRepoDefaultBranch = async (repoUrl) => {
-		try {
-			const branch = await axios.get(repoUrl, {
-				headers: {
-					'Authorization': `token ${access_token}`
-				}
-			});
-			return branch.data.default_branch
-		} catch ( error ) {
-			console.log(error)
-		}
-	}
+  const getRepoDefaultBranch = async (repoUrl) => {
+    try {
+      const branch = await axios.get(repoUrl, {
+        headers: {
+          Authorization: `token ${access_token}`,
+        },
+      });
+      return branch.data.default_branch;
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-	const getRepoTree = async (treeUrl) => {
-		try {
-			const tree = await axios.get(treeUrl, {
-				headers: {
-					'Authorization': `token ${access_token}`
-				}
-			});
-			for (const item of tree.data.tree) {
-				const ext = item.path.split('.').pop()
-				!!ext && files.push(ext)
-			}
-		} catch ( error ) {
-			console.log(error)
-		}
-	}
+  const getRepoTree = async (treeUrl) => {
+    try {
+      const tree = await axios.get(treeUrl, {
+        headers: {
+          Authorization: `token ${access_token}`,
+        },
+      });
+      for (const item of tree.data.tree) {
+        const ext = item.path.split(".").pop();
+        !!ext && files.push(ext);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-	const getRepoData = async (repoUrl) => {
-		const branch = await getRepoDefaultBranch(repoUrl)
-		await getRepoTree(`${repoUrl}/git/trees/${branch}`)
-		return files
-	};
+  const getRepoData = async (repoUrl) => {
+    const branch = await getRepoDefaultBranch(repoUrl);
+    await getRepoTree(`${repoUrl}/git/trees/${branch}`);
+    return files;
+  };
 
-	return {
-		getRepoData,
-	};
+  return {
+    getRepoData,
+  };
 };
