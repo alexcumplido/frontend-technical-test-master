55e44515634f6e49a4cd34c98d74d94ca7ef4979
Alexandre Cumplido Bou <acumpbo@gmail.com>
2025-05-02 23:44:00 +0200

    Created centralized constant variables

---
+++ src/constants/constants.js
@@ -0,0 +1,5 @@
+export const ACCESS_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+export const GITHUB_API_URL = "https://api.github.com";
+export const GITHUB_API_REPO_URL = `${GITHUB_API_URL}/repos/`;
+export const TYPE_BLOB = "blob";
+export const TYPE_TREE = "tree";
