fa832c26bae317bd4eddbfc2c7bb77eceab740a2
Alexandre Cumplido Bou <acumpbo@gmail.com>
2025-05-02 14:14:38 +0200

    Made error message empty if validation true

--- src/clients/clientValidator.jsx
+++ src/clients/clientValidator.jsx
@@ -1,15 +1,17 @@
 import validator from "validator";
 
 export const validate = (value, setError) => {
+  const urlBase = "https://api.github.com/repos/";
   const isValidUrl = validator.isURL(value, {
     protocols: ["https"],
     require_protocol: true,
     require_host: true,
   });
 
-  const isSpecific = value.startsWith("https://api.github.com/repos/");
+  const isSpecific = value.startsWith(urlBase);
 
   if (isValidUrl && isSpecific) {
+    setError("");
     return true;
   } else {
     setError("Is Not Valid URL");
