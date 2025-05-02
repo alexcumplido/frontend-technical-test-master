cdd8f9de0863d3189ec9cde9ab9c70f48d2cf707
Alexandre Cumplido Bou <acumpbo@gmail.com>
2025-05-02 21:57:03 +0200

    Clean up app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import { useState, useEffect } from "react";
-import search from "./assets/images/search.svg";
 import { useAxios } from "./hooks/useAxios";
-import file from "./assets/images/file.svg";
 import { Header } from "./components/header/Header";
-import { Button } from "./components/button/Button";
-import { Input } from "./components/input/Input";
 import { List } from "./components/list/List";
-import { Icon } from "./components/icon/Icon";
 import { validate } from "./clients/clientValidator";
-import { InputError } from "./components/inputError/InputError";
+import { Form } from "./components/form/Form";
 
 function App() {
   const [results, setResults] = useState([]);
@@ -38,23 +33,20 @@ function App() {
 
   return (
     <section className="component">
-      <Header className={"header"} />
-      <form className="form" onSubmit={handleSubmit}>
-        <Input
-          value={inputValue}
-          onChange={setInputValue}
-          placeholder={"https://api.github.com/repos/[USER]/[REPO]"}
-          className={"form__input"}
-        />
-        <InputError className={"form__error-show"} text={errorMessage} />
-
-        <Button type={"submit"} className={"form__submit"}>
-          <Icon src={search} sizes={21} text={"Search"} />
-        </Button>
-      </form>
-      <List elements={filteredList} className={"table-data"}>
-        <Icon src={file} sizes={21} text={"Search"} />
-      </List>
+      <Header
+        title={"Github Extension Counter"}
+        text={"Write the URL of the repository to analyze"}
+        className={"header"}
+      />
+      <Form
+        handleSubmit={handleSubmit}
+        inputValue={inputValue}
+        setInputValue={setInputValue}
+        errorMessage={errorMessage}
+        placeholder={"https://api.github.com/repos/[USER]/[REPO]"}
+        className={"form"}
+      />
+      <List elements={filteredList} className={"table-data"} />
     </section>
   );
 }
