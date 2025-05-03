d0a4e9448ad4600bfa4a49bbbf644202c9ca391b
Alexandre Cumplido Bou <acumpbo@gmail.com>
2025-05-03 23:38:15 +0200

    Add new constants for placeholder

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,6 @@
 export const ACCESS_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 export const GITHUB_API_URL = "https://api.github.com";
 export const GITHUB_API_REPO_URL = `${GITHUB_API_URL}/repos/`;
+export const GITHUB_API_PLACHOLDER = `${GITHUB_API_REPO_URL}/[USER]/[REPO]`;
 export const TYPE_BLOB = "blob";
 export const TYPE_TREE = "tree";
