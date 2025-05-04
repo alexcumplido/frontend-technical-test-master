9ee355a47c26cdb7d7cad4d2b99611e0952e6933
Alexandre Cumplido Bou <acumpbo@gmail.com>
2025-05-04 02:39:45 +0200

    Adapted imports and deleted non used files

--- src/requests/clientAxios.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-import { ACCESS_TOKEN } from "../constants/constants.js";
-
-if (!ACCESS_TOKEN) throw new Error(`ACESS TOKEN : ${ACCESS_TOKEN}.`);
-
-export const clientAxios = axios.create({
-  headers: { Authorization: `token ${ACCESS_TOKEN}` },
-});
--- src/utils/utils.js
+++
@@ -1,8 +0,0 @@
-export const groupItems = (items) => {
-  return items.reduce((acc, val) => {
-    acc[val] = acc[val] === undefined ? 1 : (acc[val] += 1);
-    return acc;
-  }, {});
-};
-
-export const getFileExtension = (item) => item.path.split(".").pop();
